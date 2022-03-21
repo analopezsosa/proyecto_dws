566759a281cc57f9c8ed74e89963ad8b953f230d
analopezsosa <100412590+analopezsosa@users.noreply.github.com>
2022-03-21 11:19:56 +0100

    Initial commit

---
+++ src/main/java/com/example/proyecto_dws/ProyectoDwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_dws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proyecto_dws/ProyectoDwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_dws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
