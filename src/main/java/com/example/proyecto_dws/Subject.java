f245dbfeca94d6d0f75e0a263eb2fb2e3f207bcf
marinaparram <mapamarina@gmail.com>
2022-03-21 12:49:04 +0100

    Crear las clases base Grade, Subject y User

--- src/main/java/com/example/proyecto_dws/Subject.java
+++ src/main/java/com/example/proyecto_dws/Subject.java
@@ -1,4 +1,39 @@
 package com.example.proyecto_dws;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Subject {
+    private String name;
+    private int subjectNumber;
+    private long id =-1;
+
+    public Subject(String name, int subjectNumber){
+        this.name = name;
+        this.subjectNumber = subjectNumber;
+    }
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public int getSubjectNumber() {
+        return subjectNumber;
+    }
+    public void setSubjectNumber(int subjectNumber) {
+        this.subjectNumber = subjectNumber;
+    }
+    public String getName() {return name;}
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String toString(){
+        return "Asignatura: "+name + " Número: "+ subjectNumber + ". Con ID: " + id;
+    }
 }
