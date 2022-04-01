3d88e65c54729b9ba1a3a07a16b40a44171b9316
NicoleReyesRiv <100412750+NicoleReyesRiv@users.noreply.github.com>
2022-04-01 12:37:44 +0200

    ...

--- src/main/java/com/example/proyecto_dws/UserController.java
+++ src/main/java/com/example/proyecto_dws/UserController.java
@@ -4,13 +4,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
+    @Autowired
+    private UserRepository repository;
 
-
+    @GetMapping("/viewusers.html")
+    public String showUsers(Model model){
+        model.addAttribute("users",repository.findAll());
+        return "viewgrades";
+    }
+    @PostMapping("/createusers.html")
+    public String createUser(Model model, User user){
+        repository.save(user);
+        return "user_created";
+    }
+    @GetMapping("/user/{id}")
+    public String showUser(Model model, @PathVariable long id) {
+        User user = repository.findById(id).get();
+        model.addAttribute("user", user);
+        return "view_user";
+    }
     /*
     @Autowired
     UserHolder userHolder;
