1b4fb60dc61f076e07ee9e4b99a5aa140867533a
NicoleReyesRiv <100412750+NicoleReyesRiv@users.noreply.github.com>
2022-04-18 17:43:46 +0200

    más del user

---
+++ src/main/java/com/example/proyecto_dws/UserRestController.java
@@ -0,0 +1,65 @@
+package com.example.proyecto_dws;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RequestMapping("/api")
+@RestController
+public class UserRestController {
+
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    GradeRepository gradeRepository;
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/user")
+    public ResponseEntity<Collection>users(){
+        return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<User> addUser(@RequestBody User user){
+        if(user != null){
+            userRepository.save(user);
+            return new ResponseEntity<>(user,HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+
+    }
+    @PutMapping("user/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable long id,@RequestBody User user){
+        User aux = userRepository.getById(id);
+
+        userService.updateUser(aux);
+
+
+        if(aux != null){
+            return new ResponseEntity<>(aux, HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable long id){
+        User aux = userRepository.getById(id);
+        userService.removeUser(id);
+        if (aux != null){
+            return new ResponseEntity<>(aux,HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+}
