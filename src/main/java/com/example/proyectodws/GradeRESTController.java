4f3f60d79d499021906433c181f16dd4aa96a9b2
analopezsosa <100412590+analopezsosa@users.noreply.github.com>
2022-04-28 20:16:21 +0200

    RESTController

--- src/main/java/com/example/proyectodws/GradeRESTController.java
+++ src/main/java/com/example/proyectodws/GradeRESTController.java
@@ -14,18 +14,14 @@ public class GradeRESTController {
     @Autowired
     private GradeService gradeService;
 
-    @Autowired
-
-    private GradeRepository repository;
 
     @GetMapping("/grades")
-    public ResponseEntity<Collection> gradeList(){return new ResponseEntity<>(repository.findAll(),HttpStatus.OK);}
+    public ResponseEntity<Collection> gradeList(){return new ResponseEntity<>( gradeService.gradeList(),HttpStatus.OK);}
 
     @GetMapping("/grades/{id}")
     public ResponseEntity<Grade> viewGrade(@PathVariable long id){
-        Optional<Grade> op = repository.findById(id);
-        if(op.isPresent()){
-            Grade grade = op.get();
+       Grade grade=gradeService.getGrade(id);
+        if(grade!=null){
             return new ResponseEntity<>(grade,HttpStatus.OK);
         }else{
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -34,7 +30,7 @@ public class GradeRESTController {
 
     @PostMapping("/grades")
     public ResponseEntity<Grade> addGrade(@RequestBody Grade grade){
-        repository.save(grade);
+        gradeService.addGrade(grade);
         if(grade != null){
             return new ResponseEntity<>(grade, HttpStatus.OK);
         }
--- src/main/java/com/example/proyectodws/UserRestController.java
+++ src/main/java/com/example/proyectodws/UserRestController.java
@@ -15,48 +15,47 @@ public class UserRestController {
     UserRepository userRepository;
     @Autowired
     UserService userService;
-/*
+
     @GetMapping("/user")
-    public ResponseEntity<Collection>users(){
+    public ResponseEntity<Collection> users() {
         return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
     }
 
     @PostMapping("/user")
-    public ResponseEntity<User> addUser(@RequestBody User user){
-        if(user != null){
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        if (user != null) {
             userRepository.save(user);
-            return new ResponseEntity<>(user,HttpStatus.OK);
-        }else{
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
 
     }
+/*
     @PutMapping("user/{id}")
-    public ResponseEntity<User> updateUser(@PathVariable long id,@RequestBody User user){
+    public ResponseEntity<User> updateUser(@PathVariable long id, @RequestBody User user) {
         User aux = userRepository.getById(id);
 
         userService.updateUser(aux);
 
 
-        if(aux != null){
+        if (aux != null) {
             return new ResponseEntity<>(aux, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        else{
+    }*/
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable String user) {
+        User aux = userRepository.getById(user);
+        userService.removeUser(user);
+        if (aux != null) {
+            return new ResponseEntity<>(aux, HttpStatus.OK);
+        } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
-    @DeleteMapping("/user/{id}")
-    public ResponseEntity<User> deleteUser(@PathVariable long id){
-        User aux = userRepository.getById(id);
-        userService.removeUser(id);
-        if (aux != null){
-            return new ResponseEntity<>(aux,HttpStatus.OK);
-        }
-        else{
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-*/
 }
