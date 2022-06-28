cfae29c3e349c1fbd5330c7e977b3f752ddeda3c
analopezsosa <100412590+analopezsosa@users.noreply.github.com>
2022-06-28 11:39:03 +0200

    una cosa

--- src/main/java/com/example/proyectodws/SecurityConfiguration.java
+++ src/main/java/com/example/proyectodws/SecurityConfiguration.java
@@ -1,9 +1,12 @@
 package com.example.proyectodws;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
@@ -13,5 +16,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected  void configure(HttpSecurity seguridad) throws Exception{
         seguridad.csrf().disable();
+    }
+
+    @Configuration
+    public class Encoder {
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
     }
 }
